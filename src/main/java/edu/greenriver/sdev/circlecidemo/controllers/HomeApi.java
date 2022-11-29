39113b11b1c308f0fdbf1e944c6e66a6a372fe4d
reynoyas <yasilizbeth@gmail.com>
2022-11-29 14:09:04 -0800

    Updated files

---
+++ src/main/java/edu/greenriver/sdev/circlecidemo/controllers/HomeApi.java
@@ -0,0 +1,14 @@
+package edu.greenriver.sdev.circlecidemo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/home")
+public class HomeApi {
+    @GetMapping
+    public String home(){
+        return "Hello, world!";
+    }
+}
--- src/test/java/edu/greenriver/sdev/circlecidemo/CircleCiDemoApplicationTests.java
+++ src/test/java/edu/greenriver/sdev/circlecidemo/CircleCiDemoApplicationTests.java
@@ -1,13 +1,38 @@
 package edu.greenriver.sdev.circlecidemo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.*;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CircleCiDemoApplicationTests {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate template;
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    public void homeTest(){
+        //create a request for http://localhost<port>/api/v1/home
+        String url = "http://localhost:" + port + "/api/v1/home";
+
+        HttpEntity request = new HttpEntity(new HttpHeaders());
+
+        ResponseEntity<String> response = template.exchange(url, HttpMethod.GET,
+                request, String.class);
+
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+        assertEquals(response.getBody(), "Hello, world!");
+    }
 }
