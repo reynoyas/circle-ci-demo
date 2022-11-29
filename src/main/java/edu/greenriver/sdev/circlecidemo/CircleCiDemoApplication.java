0ce5c5aaa3de455399779651723bc7022d1f641a
reynoyas <yasilizbeth@gmail.com>
2022-11-29 13:44:37 -0800

    Initial Commit

---
+++ src/main/java/edu/greenriver/sdev/circlecidemo/CircleCiDemoApplication.java
@@ -0,0 +1,13 @@
+package edu.greenriver.sdev.circlecidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleCiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CircleCiDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/greenriver/sdev/circlecidemo/CircleCiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.greenriver.sdev.circlecidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircleCiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
